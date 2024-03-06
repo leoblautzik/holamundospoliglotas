b3ac749a512f6f1e05065bc3a6ad884d801dda90
Leonardo Blautzik <leoblautzik@gmail.com>
2024-03-06 18:35:43 -0300

    holamundos ingles, español, aleman, morse (Documentado)

--- saludos.go
+++ saludos.go
@@ -7,21 +7,25 @@ import (
 	"github.com/alwindoss/morse"
 )
 
+// saludo en inglés
 func EnIngles() {
 	fmt.Print("Te saludo en Inglés: ")
 	fmt.Println("Hello World")
 }
 
+// saludo en español
 func EnEspañol() {
 	fmt.Print("Te saludo en Español: ")
 	fmt.Println("Hola Mundo")
 }
 
+// saludo en alemán
 func EnAleman() {
 	fmt.Print("Te saludo en Alemán: ")
 	fmt.Println("Hallo Welt")
 }
 
+// saludo en cósigo morse
 func EnMorse() {
 	fmt.Print("Te saludo en Morse: ")
 
